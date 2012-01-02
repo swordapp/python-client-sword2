233922f6499f35dccdedadf0ca1b7c55d3f9ee5c
Richard Jones <richard@cottagelabs.com>
2012-01-02 13:00:32 +0000

    add on-behalf-of spec test

--- tests/spec/test_spec.py
+++ tests/spec/test_spec.py
@@ -18,3 +18,15 @@ class TestConnection(TestController):
         assert conn.sd.parsed == True
         assert conn.sd.valid == True 
         assert len(conn.sd.workspaces) == 1
+        
+    def test_02_get_service_document_on_behalf_of(self):
+        conn = Connection(SSS_URL, user_name=SSS_UN, user_pass=SSS_PW, on_behalf_of=SSS_OBO)
+        conn.get_service_document()
+        
+        # given that the client is fully functional, testing that the
+        # service document parses and is valid is sufficient.  This, obviously,
+        # doesn't test the validation routine itself.
+        assert conn.sd != None
+        assert conn.sd.parsed == True
+        assert conn.sd.valid == True 
+        assert len(conn.sd.workspaces) == 1
