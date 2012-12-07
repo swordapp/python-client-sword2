eb130893fe6aea7c1f847b9f666fbb7ae4c148b9
Richard Jones <richard@cottagelabs.com>
2012-12-07 11:37:31 +0000

    logger now only loads loggingconfig if it is in place already

--- sword2/sword2_logging.py
+++ sword2/sword2_logging.py
@@ -10,7 +10,10 @@ import os
 import logging
 import logging.config
 
-_, sword2_path, _ = imp.find_module('sword2')
+try:
+    _, sword2_path, _ = imp.find_module('sword2')
+except ImportError:
+    sword2_path = ""
 SWORD2_LOGGING_CONFIG = os.path.join(sword2_path, 'data', 'sword2_logging.conf')  # default
 
 BASIC_CONFIG = """[loggers]
@@ -36,13 +39,32 @@ args=(sys.stdout,)
 format=%(asctime)s - %(name)s - %(levelname)s - %(message)s
 """
 
-def create_logging_config(pathtologgingconf):
-    fn = open(pathtologgingconf, "w")
+def create_logging_config(pathtologgingconf=None):
+    """
+    If you want to use the sword logging configuration, you should call this to create the
+    basic logging configuration (if you haven't provided it yourself in the /data/sword2_logging.conf
+    file already.  This will write the basic config to the path provided, or to the SWORD2_LOGGING_CONFIG
+    directory if no path is provided.  Directories will be created recursively as needed.
+    """
+    # set the path to default if none is provided
+    if pathtologgingconf is None:
+        pathtologgingconf = SWORD2_LOGGING_CONFIG
+    
+    # ensure that the path exists
+    d = os.path.dirname(pathtologgingconf)
+    os.makedirs(d)
+    
+    # write the basic config to the file
+    fn = open(pathtologgingconf, "wb")
     fn.write(BASIC_CONFIG)
     fn.close()
 
-if not os.path.isfile(SWORD2_LOGGING_CONFIG):
-    create_logging_config(SWORD2_LOGGING_CONFIG)
+#if not os.path.isfile(SWORD2_LOGGING_CONFIG):
+#    create_logging_config(SWORD2_LOGGING_CONFIG)
+#
+#logging.config.fileConfig(SWORD2_LOGGING_CONFIG)
 
-logging.config.fileConfig(SWORD2_LOGGING_CONFIG)
+# when we call this module, load the logging configuration if it exists
+if os.path.isfile(SWORD2_LOGGING_CONFIG):
+   logging.config.fileConfig(SWORD2_LOGGING_CONFIG)
 
